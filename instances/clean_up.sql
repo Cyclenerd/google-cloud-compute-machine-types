0f58eec1d37f536025d89c57a1629ece6be972e3
Nils K <git@nkn-it.de>
2022-04-21 13:48:22 +0200

    europe-west6 now with c2

--- instances/clean_up.sql
+++ instances/clean_up.sql
@@ -5,13 +5,6 @@
 DELETE FROM machinetypes WHERE zone LIKE 'europe-west9-%';
 DELETE FROM disktypes    WHERE zone LIKE 'europe-west9-%';
 
-/* Delete machine types that are not available in the region
- * 2022/02/24
- * Skip compute optimized instances (C2) running in Zurich
- * Price is 0: https://cloud.google.com/skus/?currency=USD&filter=AF8B-8E84-6A88
- */
-DELETE FROM machinetypes WHERE zone LIKE 'europe-west6-%' AND name LIKE 'c2-%';
-
 /*
  * Remove disconnected data centers
  */
