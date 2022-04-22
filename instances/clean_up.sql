912578130b0864f8078d2ec9fe1cfaaac2c8265f
Nils K <git@nkn-it.de>
2022-04-22 10:46:50 +0200

    clean up europe-southwest1 and us-east5

--- instances/clean_up.sql
+++ instances/clean_up.sql
@@ -5,6 +5,14 @@
 DELETE FROM machinetypes WHERE zone LIKE 'europe-west9-%';
 DELETE FROM disktypes    WHERE zone LIKE 'europe-west9-%';
 
+/* 2022/04/22: europe-southwest1 (Madrid) */
+DELETE FROM machinetypes WHERE zone LIKE 'europe-southwest1-%';
+DELETE FROM disktypes    WHERE zone LIKE 'europe-southwest1-%';
+
+/* 2022/04/22: us-east5 (Columbus) */
+DELETE FROM machinetypes WHERE zone LIKE 'us-east5-%';
+DELETE FROM disktypes    WHERE zone LIKE 'us-east5-%';
+
 /*
  * Remove disconnected data centers
  */
