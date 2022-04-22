283074a1917f8c99d3bf3b084c67b41818f1fd05
Nils K <git@nkn-it.de>
2022-04-22 10:30:01 +0200

    images export

--- build/10_export.sh
+++ build/10_export.sh
@@ -7,8 +7,14 @@
 source "00_config.sh" || exit 9
 
 echo "» SQL Export"
-echo 'DROP TABLE IF EXISTS "instances";' > "$SQL_EXPORT" || exit 9
-sqlite3 "$DB" '.dump instances' >> "$SQL_EXPORT" || exit 9
+{
+	echo 'DROP TABLE IF EXISTS "instances";'
+	sqlite3 "$DB" '.dump instances'
+	echo 'DROP TABLE IF EXISTS "disks";'
+	sqlite3 "$DB" '.dump disks'
+	echo 'DROP TABLE IF EXISTS "images";'
+	sqlite3 "$DB" '.dump images'
+} > "$SQL_EXPORT" || exit 9
 gzip -fk "$SQL_EXPORT" || exit 9
 
 echo "» CSV Export"
