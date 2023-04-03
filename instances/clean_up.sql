ba1ada009c5e43e54cfa1ee78981cfa257d9e1f9
Nils <git@nkn-it.de>
2023-04-03 11:48:34 +0200

    me-central1 is online

--- instances/clean_up.sql
+++ instances/clean_up.sql
@@ -8,8 +8,11 @@ DELETE FROM machinetypes WHERE name LIKE 'g2-standard-%';
  * Delete not yet official 100% finished regions
  */
 
+/*
 DELETE FROM machinetypes WHERE zone LIKE 'me-central1-%';
 DELETE FROM disktypes    WHERE zone LIKE 'me-central1-%';
+*/
+
 /*
  * Remove disconnected data centers
  */
