50f48e0f1f145c95deedd92114351a6d2f8e0967
Nils <git@nkn-it.de>
2023-05-08 17:18:29 +0200

    history

--- build/11_test.sh
+++ build/11_test.sh
@@ -8,15 +8,15 @@ source "00_config.sh" || exit
 
 # Check TODOs
 if grep 'TODO' < "$CSV_EXPORT"; then
-	echo "🔥 ERROR: There are still TODOs"
+	echo "ERROR: There are still TODOs"
 	exit 9
 fi
 
 # Check costs
 # n2-standard-8 in europe-west4 with SUD : 249
 if ! cat "$CSV_EXPORT" | grep 'n2-standard-8,' | grep 'europe-west4,' | head -n 1 | grep ',249' > /dev/null; then
-	echo "🔥 ERROR: n2-standard-8 in europe-west4 with cost 249 not found"
+	echo "ERROR: n2-standard-8 in europe-west4 with cost 249 not found"
 	exit 9
 fi
 
-echo "✅ DONE"
+echo "DONE"
