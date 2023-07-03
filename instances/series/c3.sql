70067104e0f13ede9ea535408e02cb20b08beb51
Nils <git@cyclenerd.de>
2023-07-03 14:51:24 +0200

    c3 bandwidth fix

--- instances/series/c3.sql
+++ instances/series/c3.sql
@@ -1,22 +1,21 @@
 /* C3 Compute-optimized */
-/* https://cloud.google.com/compute/docs/machine-types#machine_type_comparison */
-/* TODO: Check again if GA */
+/* https://cloud.google.com/compute/docs/general-purpose-machines#c3-standard_1 */
 UPDATE instances SET
 series      = 'c3',
 family      = 'General-purpose',
 cpuPlatform = 'Sapphire Rapids',
-localSsd    = '1',
+localSsd    = '0',
 bandwidth   = '32',
 spot        = '1'
 WHERE name LIKE 'c3-%';
 
-UPDATE instances SET bandwidth = '32'  WHERE name LIKE 'c3-highcpu-4';
-UPDATE instances SET bandwidth = '32'  WHERE name LIKE 'c3-highcpu-8';
-UPDATE instances SET bandwidth = '32'  WHERE name LIKE 'c3-highcpu-22';
-UPDATE instances SET bandwidth = '32'  WHERE name LIKE 'c3-highcpu-44';
-UPDATE instances SET bandwidth = '62'  WHERE name LIKE 'c3-highcpu-88';
-UPDATE instances SET bandwidth = '100' WHERE name LIKE 'c3-highcpu-176';
+UPDATE instances SET bandwidth = '32'  WHERE name LIKE 'c3-%-4';
+UPDATE instances SET bandwidth = '32'  WHERE name LIKE 'c3-%-8';
+UPDATE instances SET bandwidth = '32'  WHERE name LIKE 'c3-%-22';
+UPDATE instances SET bandwidth = '32'  WHERE name LIKE 'c3-%-44';
+UPDATE instances SET bandwidth = '62'  WHERE name LIKE 'c3-%-88';
+UPDATE instances SET bandwidth = '100' WHERE name LIKE 'c3-%-176';
 
-UPDATE instances SET tier1 = '50'  WHERE name LIKE 'c3-highcpu-44';
-UPDATE instances SET tier1 = '100' WHERE name LIKE 'c3-highcpu-88';
-UPDATE instances SET tier1 = '200' WHERE name LIKE 'c3-highcpu-176';
+UPDATE instances SET tier1 = '50'  WHERE name LIKE 'c3-%-44';
+UPDATE instances SET tier1 = '100' WHERE name LIKE 'c3-%-88';
+UPDATE instances SET tier1 = '200' WHERE name LIKE 'c3-%-176';
