e38f6719bddaf4927e4d06390b4181b236952c65
Nils <git@cyclenerd.de>
2023-11-29 15:54:11 +0100

    c2d bandwidth fix

--- instances/series/c2d.sql
+++ instances/series/c2d.sql
@@ -9,6 +9,9 @@ localSsd    = '1',
 bandwidth   = '32',
 spot        = '1'
 WHERE name LIKE 'c2d-%';
+UPDATE instances SET bandwidth = '10'  WHERE name LIKE 'c2d-%-2';
+UPDATE instances SET bandwidth = '10'  WHERE name LIKE 'c2d-%-4';
+UPDATE instances SET bandwidth = '16'  WHERE name LIKE 'c2d-%-8';
 UPDATE instances SET tier1 = '50'  WHERE name LIKE 'c2d-%-32';
 UPDATE instances SET tier1 = '50'  WHERE name LIKE 'c2d-%-56';
 UPDATE instances SET tier1 = '100' WHERE name LIKE 'c2d-%-112';
