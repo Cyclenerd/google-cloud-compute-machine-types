65974f96473a3fa402f0c389c87840b97155e16e
Nils <git@cyclenerd.de>
2023-12-15 09:11:35 +0100

    a3 fix

--- instances/clean_up.sql
+++ instances/clean_up.sql
@@ -5,6 +5,7 @@
 DELETE FROM machinetypes WHERE name LIKE 'ct4p-%';
 DELETE FROM machinetypes WHERE name LIKE 'ct5l-%';
 DELETE FROM machinetypes WHERE name LIKE 'ct5lp-%';
+DELETE FROM machinetypes WHERE name LIKE 'a3-%';
 
 /* 
  * Delete not yet official 100% finished regions
