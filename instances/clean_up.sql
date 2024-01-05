d44a2574c5e75d73bc66590089b4fb31fa239dd8
Nils <git@cyclenerd.de>
2024-01-05 08:48:29 +0100

    ct5p

--- instances/clean_up.sql
+++ instances/clean_up.sql
@@ -5,6 +5,7 @@
 DELETE FROM machinetypes WHERE name LIKE 'ct4p-%';
 DELETE FROM machinetypes WHERE name LIKE 'ct5l-%';
 DELETE FROM machinetypes WHERE name LIKE 'ct5lp-%';
+DELETE FROM machinetypes WHERE name LIKE 'ct5p-%';
 DELETE FROM machinetypes WHERE name LIKE 'a3-%';
 
 /* 
