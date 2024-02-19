65a4f087f3f0487b36e52fb8a1e7feb455180bf8
Nils <git@cyclenerd.de>
2024-02-19 10:41:01 +0100

    z3

---
+++ instances/series/z3.sql
@@ -0,0 +1,12 @@
+/* Z3 Storage-optimized */
+/* https://cloud.google.com/compute/docs/storage-optimized-machines */
+UPDATE instances SET
+series      = 'z3',
+family      = 'Storage-optimized',
+cpuPlatform = 'Sapphire Rapids',
+localSsd    = '1',
+spot        = '1'
+WHERE name LIKE 'z3-%';
+
+UPDATE instances SET bandwidth = '62',  tier1 = '100' WHERE name LIKE 'z3-highmem-88';
+UPDATE instances SET bandwidth = '100', tier1 = '200' WHERE name LIKE 'z3-highmem-176';
