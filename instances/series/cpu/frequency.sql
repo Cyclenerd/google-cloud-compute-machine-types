030eb3c7da0ef4ae2e84a2b26536e0f87ec87247
Nils <git@cyclenerd.de>
2024-02-19 12:09:49 +0100

    z3 cpu clock

--- instances/series/cpu/frequency.sql
+++ instances/series/cpu/frequency.sql
@@ -38,6 +38,7 @@ UPDATE instances SET cpuBaseClock  = '2.6', cpuTurboClock  = '3.4', cpuSingleMax
 UPDATE instances SET cpuBaseClock  = '1.9', cpuTurboClock  = '3.0', cpuSingleMaxTurboClock = '3.3' WHERE availableCpuPlatform LIKE "%Sapphire Rapids%" AND series LIKE "a3";
 UPDATE instances SET cpuBaseClock  = '1.9', cpuTurboClock  = '3.0', cpuSingleMaxTurboClock = '3.3' WHERE availableCpuPlatform LIKE "%Sapphire Rapids%" AND series LIKE "c3";
 UPDATE instances SET cpuBaseClock  = '1.9', cpuTurboClock  = '3.0', cpuSingleMaxTurboClock = '3.3' WHERE availableCpuPlatform LIKE "%Sapphire Rapids%" AND series LIKE "h3";
+UPDATE instances SET cpuBaseClock  = '1.9', cpuTurboClock  = '3.0', cpuSingleMaxTurboClock = '3.3' WHERE availableCpuPlatform LIKE "%Sapphire Rapids%" AND series LIKE "z3";
 
 /* AMD */
 /* https://cloud.google.com/compute/docs/cpu-platforms#amd_cpu_processors */
