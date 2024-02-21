57959861a47b8259a19e85eddc6f85b744531601
Nils <git@cyclenerd.de>
2024-02-21 17:56:56 +0100

    history removed

--- build/history.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-source "00_config.sh" || exit 9
-
-CSV_EXPORT="costs-$(date +'%Y-%m-%d').csv"
-
-sqlite3     \
-	-header \
-	-csv "$DB" < "history.sql" > "$CSV_EXPORT" || exit 9
