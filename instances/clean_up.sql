e098b70015294dfd97bf8079b15135f65da0717b
Nils <git@cyclenerd.de>
2024-09-21 13:12:32 +0200

    delete instances without price

--- instances/clean_up.sql
+++ instances/clean_up.sql
@@ -11,10 +11,6 @@ DELETE FROM machinetypes WHERE name LIKE 'ct5p-%';
 
 DELETE FROM machinetypes WHERE name LIKE 'x4-%';
 
-/* https://github.com/Cyclenerd/google-cloud-compute-machine-types/issues/27 */
-DELETE FROM machinetypes WHERE name LIKE 'a3-megagpu-%' AND region LIKE 'us-central1';
-DELETE FROM machinetypes WHERE name LIKE 'a3-megagpu-%' AND region LIKE 'us-west1';
-
 /* 
  * Delete not yet official 100% finished regions
  */
