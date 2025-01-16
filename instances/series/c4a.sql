d6f795e248864a9e073c3917add4fdc5ba1ef356
Nils <git@cyclenerd.de>
2025-01-16 17:27:24 +0100

    c4a lssd

--- instances/series/c4a.sql
+++ instances/series/c4a.sql
@@ -8,17 +8,19 @@ localSsd    = '0',
 sud         = '0',
 spot        = '1'
 WHERE name LIKE 'c4a-%';
-UPDATE instances SET bandwidth = '10' WHERE name LIKE 'c4a-%-1';
-UPDATE instances SET bandwidth = '10' WHERE name LIKE 'c4a-%-2';
-UPDATE instances SET bandwidth = '23' WHERE name LIKE 'c4a-%-4';
-UPDATE instances SET bandwidth = '23' WHERE name LIKE 'c4a-%-8';
-UPDATE instances SET bandwidth = '23' WHERE name LIKE 'c4a-%-16';
-UPDATE instances SET bandwidth = '23' WHERE name LIKE 'c4a-%-32';
-UPDATE instances SET bandwidth = '34' WHERE name LIKE 'c4a-%-48';
-UPDATE instances SET bandwidth = '45' WHERE name LIKE 'c4a-%-64';
-UPDATE instances SET bandwidth = '50' WHERE name LIKE 'c4a-%-72';
+UPDATE instances SET bandwidth = '10' WHERE name LIKE 'c4a-%-1%';
+UPDATE instances SET bandwidth = '10' WHERE name LIKE 'c4a-%-2%';
+UPDATE instances SET bandwidth = '23' WHERE name LIKE 'c4a-%-4%';
+UPDATE instances SET bandwidth = '23' WHERE name LIKE 'c4a-%-8%';
+UPDATE instances SET bandwidth = '23' WHERE name LIKE 'c4a-%-16%';
+UPDATE instances SET bandwidth = '23' WHERE name LIKE 'c4a-%-32%';
+UPDATE instances SET bandwidth = '34' WHERE name LIKE 'c4a-%-48%';
+UPDATE instances SET bandwidth = '45' WHERE name LIKE 'c4a-%-64%';
+UPDATE instances SET bandwidth = '50' WHERE name LIKE 'c4a-%-72%';
 
 UPDATE instances SET tier1 = '50'  WHERE name LIKE 'c4a-%-32%';
 UPDATE instances SET tier1 = '50'  WHERE name LIKE 'c4a-%-48%';
 UPDATE instances SET tier1 = '75'  WHERE name LIKE 'c4a-%-64%';
 UPDATE instances SET tier1 = '100' WHERE name LIKE 'c4a-%-72%';
+
+UPDATE instances SET localSsd = '1' WHERE name LIKE 'c4a-%-lssd';
