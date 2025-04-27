c854b32b3723849377e38e4462e12f376204327b
Nils <git@cyclenerd.de>
2025-04-27 10:03:14 +0200

    more hana

--- instances/series/sap/hana.sql
+++ instances/series/sap/hana.sql
@@ -1,6 +1,18 @@
 /* SAP HANA certified machine types */
 /* https://cloud.google.com/solutions/sap/docs/certifications-sap-hana#hana-cert-table-vms */
 
+UPDATE instances SET hana = '1' WHERE name LIKE 'c3-highmem-88';
+UPDATE instances SET hana = '1' WHERE name LIKE 'c3-standard-44';
+UPDATE instances SET hana = '1' WHERE name LIKE 'c3-highmem-44';
+UPDATE instances SET hana = '1' WHERE name LIKE 'c3-highmem-88';
+UPDATE instances SET hana = '1' WHERE name LIKE 'c3-highmem-176';
+UPDATE instances SET hana = '1' WHERE name LIKE 'c3-highmem-192-metal';
+
+UPDATE instances SET hana = '1' WHERE name LIKE 'c4-highmem-32';
+UPDATE instances SET hana = '1' WHERE name LIKE 'c4-highmem-48';
+UPDATE instances SET hana = '1' WHERE name LIKE 'c4-highmem-96';
+UPDATE instances SET hana = '1' WHERE name LIKE 'c4-highmem-192';
+
 UPDATE instances SET hana = '1' WHERE name LIKE 'n1-highmem-32';
 UPDATE instances SET hana = '1' WHERE name LIKE 'n1-highmem-64';
 UPDATE instances SET hana = '1' WHERE name LIKE 'n1-highmem-96';
@@ -17,3 +29,5 @@ UPDATE instances SET hana = '1' WHERE name LIKE 'm1-%';
 UPDATE instances SET hana = '1' WHERE name LIKE 'm2-%';
 
 UPDATE instances SET hana = '1' WHERE name LIKE 'm3-%';
+
+UPDATE instances SET hana = '1' WHERE name LIKE 'm4-%';
